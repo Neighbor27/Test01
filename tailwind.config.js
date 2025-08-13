68ad30ec19c278884ee383844329bd452fe6352f
Neighbor27 <bppcppdpp@gmail.com>
2025-08-13 21:57:07 +0900

    폰트 다시 고정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,12 @@ module.exports = {
     './*.{html,js}',
     './src/**/*.{html,js,css}',
   ],
-  theme: {
-    extend: {},
+   theme: {
+    extend: {
+      fontFamily: { // ⬇️⬇️ 이 부분을 추가하세요! ⬇️⬇️
+        sans: ['Montserrat', 'Spline Sans', 'Noto Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
